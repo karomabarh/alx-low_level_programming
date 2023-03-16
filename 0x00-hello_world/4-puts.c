fa97eca6eb95fe957f14edd79c4fea1d9af055d0
ikramb barhoun <ibarhoun@gmail.com>
2023-03-16 13:40:01 -0700

    my 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n")0;
+#include <stdio.h>
+
+int main() 
+{
+    char str[] = "Programming is like building a multilingual puzzle\n";
+    puts(str);
+    return 0;
+}
