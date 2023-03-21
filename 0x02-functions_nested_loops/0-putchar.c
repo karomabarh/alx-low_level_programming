ddafa2cb306fe04ef5d5e94f52f562bc1659cb3f
ikramb barhoun <ibarhoun@gmail.com>
2023-03-21 07:59:25 -0700

    my  0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point.
- * Description: write a program that prints "_putchar" followed by a new line.
- * Return: Always 0 (Success.)
+ * main - print Holberton, followed by new line
+ * Return: 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -19,4 +18,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-}
+}}
