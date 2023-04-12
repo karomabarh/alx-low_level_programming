9e9a8f2083afd0fed1f129a38c4565d51c347134
ikramb barhoun <ibarhoun@gmail.com>
2023-04-12 05:14:56 -0700

    kk

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup -> Newly allocated space in memory
+ *
+ * @str: Input String
+ *
+ * Return: Depend Condition
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	new = (char *) malloc(len + 1 * sizeof(char));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		new[i] = str[i];
+	}
+	new[i] = '\0';
+	return (new);
+}
